ac9d43ff795c30fc26431ec6bed3950c1025db09
Younes <youneselalaouielrhoul@gmail.com>
2026-02-03 17:04:00 +0100

    update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,19 +1,12 @@
 package com.example.demo;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication implements CommandLineRunner {
+public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
-	}
-
 }
